cdb4fb4878ddac01aec41d1f7a11143d85706e34
adoleraal <ajdoleraalmaida@gmail.com>
2024-11-19 10:37:00 +0100

    Agregar mensaje de correción de bug

--- Assets/Script/Launcher.cs
+++ Assets/Script/Launcher.cs
@@ -13,6 +13,7 @@ public class Launcher : MonoBehaviourPunCallbacks
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Correción de bug aplicada");
         PhotonNetwork.JoinRandomOrCreateRoom();
     }
 
